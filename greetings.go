1233d131b19007f9c65c8ec1ce26d3d59b1c2ffc
pillai_amal <mail.vortextv@gmail.com>
2022-06-06 16:32:05 +0530

    final commit

---
+++ greetings.go
@@ -0,0 +1,41 @@
+package greetings
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func Hello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("Empty Value")
+	}
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	format := []string{
+		"Great to see you %v :)",
+		"Hi %v Welcome !",
+		"Hail, %v! Well met!",
+	}
+	return format[rand.Intn(len(format))]
+}
